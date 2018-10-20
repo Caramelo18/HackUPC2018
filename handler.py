b86f3f778b6ac94ec9811f0f85c849cc5c5e7bf2
Caramelo18 <fabiocaramelo18@gmail.com>
2018-10-20 11:00:38 +0200

    started work on handler

--- handler.py
+++ handler.py
@@ -1,28 +1,3 @@
-'''import json
-
-
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
-'''
 import json
 import os
 import sys
@@ -34,6 +9,7 @@ import requests
 
 TOKEN = os.environ['TELEGRAM_TOKEN']
 BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
+url = BASE_URL + "/sendMessage"
 
 
 def hello(event, context):
@@ -46,13 +22,20 @@ def hello(event, context):
         response = "Please /start, {}".format(first_name)
 
         if "start" in message:
-            response = "Hello {}".format(first_name)
+            start(chat_id, first_name)
+        else:
+            data = {"text": response.encode("utf8"), "chat_id": chat_id}
+            requests.post(url, data)
 
-        data = {"text": response.encode("utf8"), "chat_id": chat_id}
-        url = BASE_URL + "/sendMessage"
-        requests.post(url, data)
 
     except Exception as e:
         print(e)
 
     return {"statusCode": 200}
+
+
+def start(chat_id, first_name):
+    response = "Hello {}! \n I am CommuBot and I am here to help you with your commute. To list the stations type /list. \n To set your starting point type /setStart <name>. \n To set your destination type /setDestination <name>".format(first_name)
+    data = {"text": response.encode("utf8"), "chat_id": chat_id}
+
+    requests.post(url, data)
