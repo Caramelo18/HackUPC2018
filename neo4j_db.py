c4f152caa055b5291761f861924732d9e7dd7a37
Diogo Campos <d.diogocampos@gmail.com>
2018-10-20 16:54:05 +0200

    Get error list

--- neo4j_db.py
+++ neo4j_db.py
@@ -3,6 +3,19 @@ import os
 
 def get_shortest_path(origin, dest):
     graph = Graph(host=os.environ['NEO4J_URL'], port=os.environ['NEO4J_PORT'], user=os.environ['NEO4J_USERNAME'], password=os.environ['NEO4J_TOKEN'], secure=True)
-    query = 'MATCH (ms:Station{{name:\'{}\'}}),(cs:Station{{name:\'{}\'}}), p = shortestPath((ms)-[*]-(cs)) RETURN p'.format(origin, dest)
+    query = 'MATCH (ms:Station{{name:\'{}\'}}),(cs:Station{{name:\'{}\'}}), path = shortestPath((ms)-[*]-(cs)) RETURN path'.format(origin, dest)
 
-    return graph.run(query).data()
+    return graph.run(query).data()[0]['path']
+
+def get_error_list(path):
+    error_list = []
+    for edge in path:
+        if edge['error']:
+            info = {
+                'message': edge['error'],
+                'origin': edge.start_node['name'],
+                'destination': edge.end_node['name']
+            }
+            error_list.append(info)
+    
+    return error_list
